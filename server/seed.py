ee606b87753f1f196f5e9453be5dfc975818bcce
zahrafeisal <zahrafeisal3@gmail.com>
2025-03-12 18:17:12 +0300

    one-many & one-one rlships

--- server/seed.py
+++ server/seed.py
@@ -18,17 +18,17 @@ with app.app_context():
     db.session.commit()
 
     # 1..many relationship between Employee and Review
-    uri_2023 = Review(year=2023, summary="Great web developer!")
-    tristan_2021 = Review(year=2021, summary="Good coding skills, often late to work")
-    tristan_2022 = Review(year=2022, summary="Strong coding skills, takes long lunches")
-    tristan_2023 = Review(year=2023, summary="Awesome coding skills, dedicated worker")
+    uri_2023 = Review(year=2023, summary="Great web developer!", employee=uri)
+    tristan_2021 = Review(year=2021, summary="Good coding skills, often late to work", employee=tristan)
+    tristan_2022 = Review(year=2022, summary="Strong coding skills, takes long lunches", employee=tristan)
+    tristan_2023 = Review(year=2023, summary="Awesome coding skills, dedicated worker", employee=tristan)
     db.session.add_all([uri_2023, tristan_2021, tristan_2022, tristan_2023])
     db.session.commit()
 
     # 1..1 relationship between Employee and Onboarding
-    uri_onboarding = Onboarding(orientation=datetime.datetime(2023, 3, 27))
+    uri_onboarding = Onboarding(orientation=datetime.datetime(2023, 3, 27),  employee=uri)
     tristan_onboarding = Onboarding(
-        orientation=datetime.datetime(2020, 1, 20, 14, 30), forms_complete=True
+        orientation=datetime.datetime(2020, 1, 20, 14, 30), forms_complete=True,  employee=tristan
     )
     db.session.add_all([uri_onboarding, tristan_onboarding])
     db.session.commit()
